43a83be5b578c577dfa45467e3268a1456345b0b
delilahxwarren-design <delilahxwarren@gmail.com>
2026-05-10 17:35:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,35 +11,36 @@ KANAL_ID = 1501855557584162818
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
-piosenki = [
+utwory = [
 
+    # Imagine Dragons
     "https://open.spotify.com/track/0pqnGHJpmpxLKifKRmU6WP",
     "https://open.spotify.com/track/3LlAyCYU26dvFZBDUIMb7a",
     "https://open.spotify.com/track/1NtIMM4N0cFa1dNzN15chl",
     "https://open.spotify.com/track/62yJjFtgkhUrXktIoSjgP2",
-    "https://open.spotify.com/track/213x4gsFDm04hSqIUkg88w",
 
-    "https://open.spotify.com/track/2VxeLyX666F8uXCJ0dZF8B",
-    "https://open.spotify.com/track/7rglLriMNBPAyuJOMGwi39",
+    # Lady Gaga
     "https://open.spotify.com/track/6rLqjzGV5VMLDWEnuUqi8q",
     "https://open.spotify.com/track/5R8dQOPq8haW94K7mgERlO",
+    "https://open.spotify.com/track/0SiywuOBRcynK0uKGWdCnn",
 
+    # Sabrina Carpenter
     "https://open.spotify.com/track/2qSkIjg1o9h3YT9RAgYN75",
     "https://open.spotify.com/track/5N3hjp1WNayUPZrA8kJmJP",
-    "https://open.spotify.com/track/1d7Ptw3qYcfpdLNL5REhtJ",
 
-    "https://open.spotify.com/track/2takcwOaAZWiXQijPHIx7B",
-    "https://open.spotify.com/track/0eGsygTp906u18L0Oimnem",
-    "https://open.spotify.com/track/7ouMYWpwJ422jRcDASZB7P"
+    # Rock
+    "https://open.spotify.com/track/7ouMYWpwJ422jRcDASZB7P",
+    "https://open.spotify.com/track/58ge6dfP91o9oXMzq3XkIS",
+    "https://open.spotify.com/track/2nLtzopw4rPReszdYBJU6h"
 ]
 
 @client.event
 async def on_ready():
     print(f"{client.user} online!")
-    codzienna_piosenka.start()
+    codzienny_utwor.start()
 
 @tasks.loop(minutes=1)
-async def codzienna_piosenka():
+async def codzienny_utwor():
 
     teraz = datetime.now()
 
@@ -49,7 +50,7 @@ async def codzienna_piosenka():
 
         if kanal:
             await kanal.send(
-                f"🎶 Dzisiejsza piosenka od Dethe:\n{random.choice(piosenki)}"
+                f"🎶 Dzisiejszy utwór od Dethe:\n{random.choice(utwory)}"
             )
 
 @client.event
@@ -61,9 +62,9 @@ async def on_message(message):
     if message.content == "!ping":
         await message.channel.send("🏓 Pong!")
 
-    if message.content == "!piosenka":
+    if message.content == "!utwór":
         await message.channel.send(
-            f"🎵 Dethe poleca:\n{random.choice(piosenki)}"
+            f"🎵 Dethe poleca:\n{random.choice(utwory)}"
         )
 
 client.run(TOKEN)
