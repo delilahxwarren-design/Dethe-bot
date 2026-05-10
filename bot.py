778832cc415e1ea8695c1613a85cddb43801d201
delilahxwarren-design <delilahxwarren@gmail.com>
2026-05-10 18:46:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,29 @@ import os
 import asyncio
 from discord.ext import tasks
 from datetime import datetime
+from flask import Flask
+from threading import Thread
 
 TOKEN = os.getenv("TOKEN")
 
 KANAL_ID = 1501855557584162818
+ROLA_BUTELKA = "BUTELKA"
+
+# KEEP ALIVE DLA RENDERA 💜
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Dethe żyje 💜"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
@@ -60,28 +79,33 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    # PING
     if message.content == "!ping":
         await message.channel.send("🏓 Pong!")
 
+    # LOSOWY UTWÓR
     if message.content == "!utwór":
         await message.channel.send(
             f"🎵 Dethe poleca:\n{random.choice(utwory)}"
         )
 
+    # BUTELKA
     if message.content == "!butelka":
 
         members = [
             member for member in message.channel.members
-            if not member.bot
+            if not member.bot and any(role.name == ROLA_BUTELKA for role in member.roles)
         ]
 
         if len(members) < 2:
-            await message.channel.send("❌ Za mało osób do gry!")
+            await message.channel.send(
+                "❌ Za mało osób z rolą BUTELKA!"
+            )
             return
 
         osoba = random.choice(members)
 
-        await message.channel.send("🍾 Kręcę butelką...")
+        await message.channel.send("🍾 Dethe kręci butelką...")
         await asyncio.sleep(1)
 
         await message.channel.send("3️⃣")
@@ -93,7 +117,10 @@ async def on_message(message):
         await message.channel.send("1️⃣")
         await asyncio.sleep(1)
 
-        wybor = random.choice(["❓ PYTANIE", "🔥 WYZWANIE"])
+        wybor = random.choice([
+            "❓ PYTANIE",
+            "🔥 WYZWANIE"
+        ])
 
         await message.channel.send(
             f"🍾 Butelka wskazuje: {osoba.mention}\n\n{wybor}"
